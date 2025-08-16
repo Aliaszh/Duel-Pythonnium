79a0c2d8ff63dc7762eee7b73be5a37a6b1598e7
Aliaszh <alias.akgunv2@gmail.com>
2025-08-16 15:33:42 +0200

    Update Duel.py
    module time ajouter
    amélioration globale du code

--- Duel.py
+++ Duel.py
@@ -1,14 +1,19 @@
-import
-decision_joueur = input("Salut à toi jeune guerrier , tu viens d'entrer dans le repére du grand Pythonnium. Est tu pret a l'affronter durant un jeu de pierre feuille ciseaux ? oui ou non jeune guerrier ?: ")
+import time
+print("Salut à toi jeune guerrier , tu viens d'entrer dans le repére du grand Pythonnium.")
+time.sleep(1)
+decision_joueur = input("Est tu pret a l'affronter durant un jeu de pierre feuille ciseaux ? Oui ou Non jeune guerrier ?: ").lower()
 if decision_joueur == "oui" :
   print("Trés bien prépare toi")
 elif decision_joueur == "non" :
-  print("mauviette je savais que tu n'en etais pas capable !")
-  breakpoint
+  print("Mauviette je savais que tu n'en etais pas capable !")
+  quit()
 else :
   print("Je t'ai demandé oui ou non pas de me raconter ta vie")
-  breakpoint
-joueur = input("Que choisis tu entre la pierre , la feuille ou les ciseaux ? : ")
+  quit()
+joueur = input("Que choisis tu entre la pierre , la feuille ou les ciseaux ? : ").lower()
+time.sleep(1)
+print("Le grand Pythonnium réfléchit...")
+time.sleep(2)
 if joueur == "pierre" or joueur == "la pierre":
   ordinateur = "feuille"
   print(f"Le grand Pythonnium a joué , son choix est : {ordinateur}")
@@ -20,9 +25,11 @@ elif joueur ==  "ciseaux" or joueur == "les ciseaux" :
   print(f"Le grand Pythonnium a joué , son choix est : {ordinateur}")
 else:
   print("TU NE SAIS PAS JOUER, LE GRAND PYTHONNIUM TA ÉCRASÉ ET DÉGUSTÉ AU FORMAT BINAIRE !")
-  breakpoint
+  quit()
+time.sleep(1)
 print(f"tu avais donc choisis {joueur} et le grand Pythonnium {ordinateur}, tu as donc ...")
-sleep(3)
+time.sleep(3)
 nom = input("PERDU !!!! mais tu as le merite d'avoir essayé, quel est ton nom que je m'en souvienne ? ")
 print(f"{nom} c'est ça ? tu resteras dans les mémoires !")
-breakpoint
+quit()
+
